fd0281e6becdab9d926ea0efde7d369d21178263
Rohan <rohan.farmington@gmail.com>
2021-02-26 22:36:57 -0500

    Landing page

--- pomo-frontend/src/App.js
+++ pomo-frontend/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react'; 
 import logo from './logo.svg';
 import './App.css';
-
+import Banner from './components/Banner/Banner.jsx';
+import Navbar from './components/Navbar/Navbar.js';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
+         
+        </header> */}
+        <Navbar></Navbar>
+        <Banner>
           
-        </header>
+        
+        </Banner>
       </div>
     );
   }
---
+++ pomo-frontend/src/components/Landing/Landing.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+
+class Landing extends Component{
+
+
+
+
+
+
+    render() {
+        return (
+    
+
+        )
+    }
+
+
+}
+
+export default Landing;
